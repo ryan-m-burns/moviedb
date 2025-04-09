833801d37684643750d7835b113d1b5d4d0e45b1
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2025-04-08 17:26:28 -0700

    quick footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import './Footer.css';
+import { getCurrentYear } from '../utils/helpers';
 
 export default function Footer() {
   return (
     <footer>
-      <p>Footer</p>
+      <p>&copy;{getCurrentYear()} - Ryan Burns. All Rights Reserved.</p>
     </footer>
   );
 }
