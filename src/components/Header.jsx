16333f879ff1b0e77342282e7e354163921e380f
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2025-04-08 18:15:11 -0700

    adjust styling for header and add Links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <header>
-      <p>Header</p>
+      <div className='header-logo'>🎥</div>
+      <div className='nav-links'>
+        <Link to='/' className='nav-link'>
+          Home
+        </Link>
+        <Link to='/about' className='nav-link'>
+          About
+        </Link>
+        <Link to='/favorites' className='nav-link'>
+          Favorites
+        </Link>
+      </div>
     </header>
   );
 }
